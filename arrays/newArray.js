c03bed6543509a9d7f0c0390f45edd79106003a9
shubham <shubhamyelekar0@gmail.com>
2025-01-09 19:50:23 +0530

    Update newArray.js

--- arrays/newArray.js
+++ arrays/newArray.js
@@ -21,12 +21,36 @@ class Array {
     this.length--
     return lastItem
   }
+
+  shift(){
+    const firstEl = this.data[0]
+    for(let i = 0; i < this.length; i++){
+      this.data[i] = this.data[i+1]
+    }
+    delete this.data[this.length - 1]
+    this.length--
+    return firstEl
+  }
+
+  delete(index){
+    const item = this.data[index];
+    for(let i=index; i<this.length - 1; i++){
+      this.data[i] = this.data[i+1]
+    }
+
+    delete this.data[this.length - 1]
+    this.length--
+    return item
+  }
 }
 
 const newArray = new Array()
 newArray.push("Mango")
 newArray.push("Ball")
 newArray.push("Watch")
+newArray.push("Car")
+newArray.push("Page")
+newArray.push("Pen")
 
-newArray.pop();
+newArray.delete(3);
 console.log(newArray)
